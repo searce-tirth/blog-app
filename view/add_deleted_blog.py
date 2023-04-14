c6faddf8df8db71b4392d9abb8ceff8ac16bf840
searce-tirth <128037747+searce-tirth@users.noreply.github.com>
2023-04-14 16:52:03 +0530

    Update add_deleted_blog.py

--- view/add_deleted_blog.py
+++ view/add_deleted_blog.py
@@ -7,8 +7,9 @@ from utilities.get_db_connection import get_db_connection
 class adddeletedBlog:
     def __init__(self, request_payload):
         self.request_payload = request_payload
+        self.USER_NAME = self.request_payload.get("user_name")
         self.USER_ID = self.request_payload.get("user_id")
-        self.BLOG_ID = self.request_payload.get("blog_id")
+        self.BLOG_NAME = self.request_payload.get("blog_name")
         
 
         self.response = Response(
@@ -26,9 +27,9 @@ class adddeletedBlog:
         #self.persist_to_db()
         try:
             self.response = Response(json.dumps({
-                "message": "Added Deleted Blog",
+                "message": "Restored Deleted Blog",
                 "data" : {
-                    "blog_id":self.BLOG_ID,
+                    "blog_name":self.BLOG_NAME,
                     "content":self.CONTENT
                 }
             }), status=200, mimetype="application/json")
@@ -48,33 +49,33 @@ class adddeletedBlog:
         dictn = (self.DB_CONNECTION.find({"id" : str(self.USER_ID)},{"blogs":1,"_id":0}))
         for i in dictn:
             for j in i["blogs"]:
-                if j["blog_id"] == (self.BLOG_ID) and j["delete_status"] == "true":
+                if j["blog_id"] == (self.BLOG_NAME) and j["delete_status"] == "true":
                     self.CONTENT = j["content"]
                     self.CREATED_AT = str(j["created_at"])
                     self.add_deleted_blog()
                     return
                 
-                elif j["blog_id"] == (self.BLOG_ID) and j["delete_status"] == "false":
+                elif j["blog_id"] == (self.BLOG_NAME) and j["delete_status"] == "false":
                     self.response = Response(json.dumps({
                     "message": "Blog is not deleted"
                     }), status=500, mimetype="application/json")
                     return
             else:
                 self.response = Response(json.dumps({
-                    "message": "You can't add this deleted blog"
+                    "message": "Incorrect user ID"
                 }), status=500, mimetype="application/json")
                 break
             
         else:
             self.response = Response(json.dumps({
-                    "message": "User doesn't exist"
+                    "message": "Incorrect user ID"
                 }), status=500, mimetype="application/json")
             
     def add_deleted_blog(self):
         self.response = Response(json.dumps({
             "message": "Can't get the blog from Database"
         }), status=500, mimetype="application/json")
-        self.DB_CONNECTION.update_one({"id" : str(self.USER_ID),"blogs.blog_id":str(self.BLOG_ID)}, {"$set" : {"blogs.$.delete_status" : "false"}})
+        self.DB_CONNECTION.update_one({"id" : str(self.USER_ID),"blogs.blog_name":str(self.BLOG_NAME)}, {"$set" : {"blogs.$.delete_status" : "false"}})
         
                 
 
